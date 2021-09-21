4bea35d411100bf08e1488864992d51cf4b042d6
gamma <edwardpara910@gmail.com>
2021-09-21 13:45:20 -0500

    Added retireval method

--- js/main.js
+++ js/main.js
@@ -21,13 +21,13 @@ function addLike(){
     editField.innerHTML = '<a id="like-edit-'+currentRow+'" >Editar</a>';
 
     
-    likeField.setAttribute('id', 'like-field-'+currentRow)
-    percentageField.setAttribute('id', 'percentage-field-'+currentRow)
-    editField.setAttribute('id', 'edit-field-'+currentRow)
+    likeField.setAttribute('id', 'like-field-'+currentRow);
+    percentageField.setAttribute('id', 'percentage-field-'+currentRow);
+    editField.setAttribute('id', 'edit-field-'+currentRow);
     
     var editText = document.getElementById('like-edit-'+currentRow);
-    editText.setAttribute('class', 'like-edit')
-    editText.setAttribute('onclick', 'editLike('+currentRow+')')
+    editText.setAttribute('class', 'like-edit');
+    editText.setAttribute('onclick', 'editLike('+currentRow+')');
 }
 
 function editLike(row){
@@ -45,17 +45,23 @@ function editLike(row){
     const oldLikeValue = likeField.innerText;
     const oldPercentageValue = percentageField.innerText;
 
-    likeField.innerHTML = '<input type="text" id="input-like-edit-'+row+'"class="form-control normal-input" value="'+oldLikeValue+'" name="gusto">'
-    percentageField.innerHTML = '<input type="text" id="input-percentage-edit-'+row+'" class="form-control normal-input" value="'+oldPercentageValue+'" name="porc">'
-    editField.innerHTML = 'En edición'
+    likeField.innerHTML = '<input type="text" id="input-like-edit-'+row+'"class="form-control normal-input" value="'+oldLikeValue+'" name="gusto">';
+    percentageField.innerHTML = '<input type="text" id="input-percentage-edit-'+row+'" class="form-control normal-input" value="'+oldPercentageValue+'" name="porc">';
+    editField.innerHTML = 'En edición';
 
     var editBar = document.getElementById('bottom-edit-div');
-    editBar.innerHTML = '<p class="subtitle-text">Pulse Aceptar para guardar los cambios o cancelar para anularlos</p><button type="submit" class="btn btn-outline-secondary">Aceptar</button><button type="button" class="btn btn-outline-danger" onclick="location.reload()">Cancelar</button>'
+    editBar.innerHTML = '<p class="subtitle-text">Pulse Aceptar para guardar los cambios o cancelar para anularlos</p><button type="submit" class="btn btn-outline-secondary">Aceptar</button><button type="button" class="btn btn-outline-danger" onclick="location.reload()">Cancelar</button>';
 
 }
 
-function acceptChanges(row){
-
+function getResults(){
     inEdition = false;
+    const requestParameters = new URLSearchParams(window.location.search);
+
+    document.getElementById("name-label").innerText = requestParameters.get("name");
+    document.getElementById("email-label").innerText = requestParameters.get("email");
+    document.getElementById("phone-label").innerText = requestParameters.get("telefono");
+    document.getElementById("like-label").innerText = requestParameters.get("gusto");
+    document.getElementById("percentage-label").innerText = requestParameters.get("perc");
 
 }
