cb63e7e454b22f9ba86770c05595861ec1b92d53
gamma <edwardpara910@gmail.com>
2021-09-21 13:57:48 -0500

    Added cancel function

--- js/main.js
+++ js/main.js
@@ -31,7 +31,6 @@ function addLike(){
 }
 
 function editLike(row){
-
     if (inEdition){
         alert("Solo se puede editar una línea. Recargue la página para poder editar otra")
         return;
@@ -50,8 +49,29 @@ function editLike(row){
     editField.innerHTML = 'En edición';
 
     var editBar = document.getElementById('bottom-edit-div');
-    editBar.innerHTML = '<p class="subtitle-text">Pulse Aceptar para guardar los cambios o cancelar para anularlos</p><button type="submit" class="btn btn-outline-secondary">Aceptar</button><button type="button" class="btn btn-outline-danger" onclick="location.reload()">Cancelar</button>';
+    editBar.innerHTML = '<p class="subtitle-text">Pulse Aceptar para guardar los cambios o cancelar para anularlos</p><button type="submit" class="btn btn-outline-secondary">Aceptar</button><button type="button" class="btn btn-outline-danger" onclick="cancelEdit('+row+', '+oldLikeValue+', '+oldPercentageValue+')">Cancelar</button>';
 
+}
+
+// Si se cancela, se ignoran los cambios y se deja la tabla como estaba
+function cancelEdit(row, likeValue, percentageValue){
+    inEdition = false;
+
+    var likeField = document.getElementById('like-field-'+row);
+    var percentageField = document.getElementById('percentage-field-'+row);
+    var editField = document.getElementById('edit-field-'+row);
+
+    likeField.innerHTML = likeValue;
+    percentageField.innerHTML = percentageValue;
+    editField.innerHTML = '<a id="like-edit-'+row+'" >Editar</a>';
+
+    likeField.setAttribute('id', 'like-field-'+row);
+    percentageField.setAttribute('id', 'percentage-field-'+row);
+    editField.setAttribute('id', 'edit-field-'+row);
+    
+    var editText = document.getElementById('like-edit-'+row);
+    editText.setAttribute('class', 'like-edit');
+    editText.setAttribute('onclick', 'editLike('+row+')');
 }
 
 function getResults(){
